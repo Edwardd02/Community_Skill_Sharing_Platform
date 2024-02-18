66f74ed3121f1bbbf613088df19cd5f593fdc36e
Edwardd02 <tup19197@temple.edu>
2024-02-18 18:54:35 -0500

    Mongoose configured

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mydatabase')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+
+// Define a schema
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get('/', (req, res) => {
